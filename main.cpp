8d5bc8f0587b52e7503ef76f262cf32a67f377e2
olegmakeienko <molva83@gmail.com>
2024-09-23 14:49:35 +0200

    noting to commit

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <ctime>
+#include <ncurses.h>
 
 #include "Game.h"
 
 using namespace std;
 
-int main()
-{
+int main() {
     //srand(time(0));
     Game game;
     game.run();
-    return 0;
 }
