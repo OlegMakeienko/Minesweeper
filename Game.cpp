92915185e937d8fc6b956402bbf3ae8600ae2947
olegmakeienko <molva83@gmail.com>
2024-09-23 10:04:47 +0200

    Added Game class implementation with logo display and game run functions.
    - Implemented gotoxy() to move the cursor to specified coordinates.
    - Added showLogo() to display the game logo using ncurses.
    - Added run() to initialize the field, place a random mine, and display the field.

---
+++ Game.cpp
@@ -0,0 +1,35 @@
+//
+// Created by OlegMakeienko on 2024-09-22.
+//
+
+#include "Game.h"
+
+#include "Field.h"
+using namespace std;
+
+#include <iostream>
+#include <unistd.h>
+#include <ncurses.h>
+
+void gotoxy(int x, int y) {
+    move(y, x);
+}
+
+void Game::showLogo() {
+    initscr();
+    gotoxy(40, 5);
+    printw("Minesweeper (by Oleg Makeienko)");
+    refresh();
+    napms(2000);
+    clear();
+    refresh();
+    endwin();
+}
+
+void Game::run() {
+    //showLogo();
+    Field field;
+    field.initField();
+    field.setRandomMines(1);
+    field.showField();
+}
---
+++ Game.h
@@ -0,0 +1,20 @@
+//
+// Created by OlegMakeienko on 2024-09-22.
+//
+
+#ifndef GAME_H
+#define GAME_H
+
+
+
+class Game {
+private:
+    void showLogo();
+
+public:
+    void run();
+};
+
+
+
+#endif //GAME_H
