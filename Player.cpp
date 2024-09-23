2bcc6ce99829d9b7f5aa93c3d9d714813e1e19e8
olegmakeienko <molva83@gmail.com>
2024-09-23 23:03:21 +0200

    control border

--- Player.cpp
+++ Player.cpp
@@ -13,21 +13,28 @@ using namespace std;
 Player::Player() {
 }
 
-void Player::choice() {
+void Player::choice(Field &field) {
     int ch = 0;
-    int x = 1;
-    int y = 1;
+    int x = 1, y = 1;
+    int prevX = 1, prevY = 1;
 
     gotoxy(x, y);
 
     while (true) {
         ch = getch();
 
+        prevX = x, prevY = y;
+
         switch (ch) {
             case KEY_RIGHT: x++; break;
             case KEY_LEFT: x--; break;
             case KEY_DOWN: y++; break;
             case KEY_UP: y--; break;
+        }
+
+        if (field.isBorder(x, y)) {
+            x = prevX;
+            y = prevY;
         }
         gotoxy(x, y);
     }
