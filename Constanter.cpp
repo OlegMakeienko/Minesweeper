8c227def3f5ed9689141d7cf94de9a272ec22269
olegmakeienko <molva83@gmail.com>
2024-09-26 10:18:46 +0200

    added constants instead to have there like privat in class Field

---
+++ Constanter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by OlegMakeienko on 2024-09-25.
+//
+
+#include "Constanter.h"
---
+++ Constanter.h
@@ -0,0 +1,13 @@
+//
+// Created by OlegMakeienko on 2024-09-25.
+//
+
+#ifndef CELLTYPE_H
+#define CELLTYPE_H
+
+const int BORDER = 100;
+const int MINE = 10;
+const int EMPTY_CELL = 0;
+const int OPEN = 1;
+
+#endif //CELLTYPE_H
